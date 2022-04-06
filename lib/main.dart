4b443bb38874749c778c7e2c33372f43ac18e828
jwilll4455 <burakceylann@outlook.com>
2022-04-06 10:10:31 +0300

    Add Uri for accessing host

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -51,6 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    get(Uri(
+      host: "pub.dev",
+      scheme: "https",
+      pathSegments: ["packages", "https", "install"],
+    ));
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
